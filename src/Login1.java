5f9c886e3511b3750fb34c69b36048ad34a90242
carlo <carlosbruninoops@gmail.com>
2025-03-07 20:18:31 -0300

    Consertei a identação

---
+++ src/Login1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Login1 {
+    public static void main(String[] args){
+        Scanner Entrar = new Scanner(System.in);
+            System.out.print("Digite seu nome: ");
+                String nome = Entrar.nextLine();
+
+        System.out.print("Digite sua idade: ");
+            int idade = Entrar.nextInt();
+                int soma =  50 - idade;
+
+        System.out.print("Digite sua altura: ");
+            Double altura = Entrar.nextDouble();
+                double alturaF = altura * 100;
+
+        if(idade >= 50){
+            System.out.println("Bem vindo(a) " + nome + "! você já possui 50 anos ou mais");
+        }
+        else{
+            System.out.println("Bem vindo(a) " + nome + "! Em "+ soma + " anos você terá 50 anos!" );
+        }
+        System.out.print("Sua altura é: " + alturaF + " centímetros" );
+        Entrar.close();
+    }
+}
